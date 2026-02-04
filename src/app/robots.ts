3d15f5e23f7f7487bc19eeaf9a84b924d7526927
wild-butterfly <askinyagmurkose@outlook.com>
2026-02-04 15:04:39 +1100

    Add sitemap and robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,9 +1,13 @@
-export default function robots() {
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
     sitemap: "https://askin-workspace.vercel.app/sitemap.xml",
   };
 }
